f6618f5ea86804b9916b41cede2cf8b1c99b8423
GickiAnarchy <mindofmagick85@gmail.com>
2022-02-17 23:55:28 -0600

    init2

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import nextcord
 from nextcord.ext import commands
 import os
-from botprivate import botprivate
+from private import botprivate
+from data.sqlite3connection import *
 
 intents = nextcord.Intents.all()
 intents.members = True
@@ -12,20 +13,19 @@ toke = botprivate.TokenClass()
 
 @client.command()
 async def loadcog(ctx, cog):
-    client.load_extension(f"cogs.{cog}")
+    client.load_extension(f"{cog}")
+    await ctx.send(f"{cog} has been loaded!")
 
 @client.command()
 async def unloadcog(ctx, cog):
     client.unload_extension(f"cogs.{cog}")
 
-@client.event()
+@client.event
 async def on_ready():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             client.load_extension(f"cogs.{filename[:-3]}")
-
-
-
+    await client.change_presence(status=nextcord.Status.online, activity=nextcord.Game("*WIP* Economy Bot -  'c!'"))
 
 #Run the f'n thing!
-bot.run(toke.getToken())
+client.run(toke.getToken())
