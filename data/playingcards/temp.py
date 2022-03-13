55b831b742a7aa47c03ad237886168a7924e8a53
GickiAnarchy <mindofmagick85@gmail.com>
2022-03-13 04:05:46 -0500

    6

--- data/playingcards/temp.py
+++ data/playingcards/temp.py
@@ -1,46 +1,86 @@
 import os
 import tkinter as tk
+import PIL
 from PIL import Image, ImageTk
-import cards
-import deck
+from cards import Card
+from deck import Deck
 import requests
 from io import BytesIO
 
+
+pathHere = os.path.abspath(os.path.dirname(__file__))
 
 class matchImages():
     def __init__(self):
         self.urls = []
         self.deck = Deck()
         self.img_dictionary = {}
-        with open("cards_img_url.txt") as r:
+        f = f"{pathHere}/cURL.txt"
+        with open(f) as r:
             for line in r:
                 self.urls.append(line)
+        self.setupWindow()
+        self.loadImage()
 
 
     def setupWindow(self):
         self.window = tk.Tk()
-        self.window.geometry("500×700")
+        self.window.geometry("800x640")
         
         self.cardLabel = tk.Label(self.window)
         self.cardLabel.pack(side = tk.TOP)
         
-        next_button = tk.Button(self.window, text ="Next", command=self.next)
-        next_button.pack(side=tk.BOTTOM, padx=(0, 20), pady=(0, 20))
-        
+        self.imgname = tk.Entry(fg="red", width=50)
+        self.imgname.pack()
+                
+        self.next_button = tk.Button(self.window, text ="Next", command=self.next)
+        self.next_button.pack(side=tk.BOTTOM, padx=(0, 20), pady=(0, 20))
+
     
     
     def loadImage(self):
+        self.tUrl = self.urls.pop()
+        response = requests.get(self.tUrl)
+        self.img = PIL.Image.open(BytesIO(response.content))
+        self.cimg = ImageTk.PhotoImage(self.img)
+        self.cardLabel.config(image = self.cimg)
+        self.cardLabel.image = self.cimg
         
-        response = requests.get(url)
-        img = Image.open(BytesIO(response.content))
-        
-        self.c1img = ImageTk.PhotoImage(Image.open(self.cd.imagePath()))
-        resize_image = c1img.resize((200, 340) , Image.ANTIALIAS)
-        img = ImageTk.PhotoImage(resize_image)
+        #resize_image = c1img.resize((200, 340) , Image.ANTIALIAS)
+        #img = ImageTk.PhotoImage(climg)
 
         
     def next(self):
-        pass
+        if self.imgname.get() == "" or self.imgname.get() == None:
+            self.errorBox()
+            return
+        self.nametext = self.imgname.get()
+        self.img_dictionary[self.nametext] = self.tUrl
+        
+        if len(self.urls) <= 0:
+            names = list(self.img_dictionary.keys())
+            sites = list(self.img_dictionary.values())
+            
+            with open("imgdict.txt", "w") as ww:
+                for x in len(self.img_dictionary):
+                    print(str(x))
+                    n = names.pop()
+                    s = sites.pop()
+                    fstr = f"{n}  __  {s}/n"
+                    ww.write(fstr)
+                    
+    
+    def errorBox(self):
+        msgbox.showinfo("Blank Name", "Must enter a name in the field.")
+        
+    def run(self):
+        self.window.mainloop()
+
+
+
+###############################################################################
+m = matchImages()
+m.run()
 
 
 
